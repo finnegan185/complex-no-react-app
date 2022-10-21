9c48e29eb1354cbdcbd06806c8b09bb7d3704dac
Zack Finnegan <zack.finnegan4@gmail.com>
2022-10-21 09:47:52 -0400

    Second Commit

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient("mongodb+srv://mainUser:RSNGly2ZwbeODgC9@cluster0.i74ongp.mongodb.net/OurAppCourse?retryWrites=true&w=majority");
+const client = new MongoClient(process.env.CONNECTIONSTRING);
 
 async function start() {
   await client.connect();
   module.exports = client.db();
   const app = require("./app");
-  app.listen(3000);
+  app.listen(process.env.PORT);
 }
 
 start();
