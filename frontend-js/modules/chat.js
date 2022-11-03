bdb478179f72a0350a9cb2761dc048b0824642e7
Zack Finnegan <zack.finnegan4@gmail.com>
2022-11-03 13:35:28 -0400

    replace my chat.js code with instructors to see if chat being shown on all page loads goes away.

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,14 +4,12 @@ export default class Chat {
   constructor() {
     this.openedYet = false;
     this.chatWrapper = document.querySelector("#chat-wrapper");
-    this.headerChatIcon = document.querySelector(".header-chat-icon");
-    this.isChatOpen = false;
+    this.openIcon = document.querySelector(".header-chat-icon");
     this.injectHTML();
     this.chatLog = document.querySelector("#chat");
     this.chatField = document.querySelector("#chatField");
     this.chatForm = document.querySelector("#chatForm");
-    this.chatTitleClose = document.querySelector(".chat-title-bar-close");
-
+    this.closeIcon = document.querySelector(".chat-title-bar-close");
     this.events();
   }
 
@@ -21,16 +19,8 @@ export default class Chat {
       e.preventDefault();
       this.sendMessageToServer();
     });
-    this.chatTitleClose.addEventListener("click", () => {
-      this.closeChat();
-    });
-    this.headerChatIcon.addEventListener("click", () => {
-      if (!this.isChatOpen) {
-        this.openChat();
-      } else {
-        this.closeChat();
-      }
-    });
+    this.openIcon.addEventListener("click", () => this.showChat());
+    this.closeIcon.addEventListener("click", () => this.hideChat());
   }
 
   // Methods
@@ -38,37 +28,33 @@ export default class Chat {
     this.socket.emit("chatMessageFromBrowser", { message: this.chatField.value });
     this.chatLog.insertAdjacentHTML(
       "beforeend",
-      DOMPurify.sanitize(
-        `
-          <div class="chat-self">
-            <div class="chat-message">
-              <div class="chat-message-inner">
-                ${this.chatField.value}
-              </div>
-            </div>
-            <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      DOMPurify.sanitize(`
+    <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
           </div>
-        `
-      )
+        </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      </div>
+    `)
     );
     this.chatLog.scrollTop = this.chatLog.scrollHeight;
     this.chatField.value = "";
     this.chatField.focus();
   }
 
-  openChat() {
-    this.chatWrapper.classList.add("chat--visible");
-    this.chatField.focus();
-    this.isChatOpen = true;
-    if (!this.openedYet) {
-      this.openConnection();
-      this.openedYet = true;
-    }
+  hideChat() {
+    this.chatWrapper.classList.remove("chat--visible");
   }
 
-  closeChat() {
-    this.chatWrapper.classList.remove("chat--visible");
-    this.isChatOpen = false;
+  showChat() {
+    if (!this.openedYet) {
+      this.openConnection();
+    }
+    this.openedYet = true;
+    this.chatWrapper.classList.add("chat--visible");
+    this.chatField.focus();
   }
 
   openConnection() {
@@ -85,17 +71,15 @@ export default class Chat {
   displayMessageFromServer(data) {
     this.chatLog.insertAdjacentHTML(
       "beforeend",
-      DOMPurify.sanitize(
-        `
-          <div class="chat-other">
-            <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
-            <div class="chat-message"><div class="chat-message-inner">
-              <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
-              ${data.message}
-            </div></div>
-          </div>
-        `
-      )
+      DOMPurify.sanitize(`
+    <div class="chat-other">
+        <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+        <div class="chat-message"><div class="chat-message-inner">
+          <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+          ${data.message}
+        </div></div>
+      </div>
+    `)
     );
     this.chatLog.scrollTop = this.chatLog.scrollHeight;
   }
@@ -104,7 +88,7 @@ export default class Chat {
     this.chatWrapper.innerHTML = `
     <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
     <div id="chat" class="chat-log"></div>
-
+    
     <form id="chatForm" class="chat-form border-top">
       <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
     </form>
